5a4ca88ce2e3eab8eec748dbfa6d763e3a310b21
calvye <calvye@192.168.16.104>
2017-07-11 21:15:51 +0800

    updatea code

--- cameltest/src/main/java/com/capgemini/microservice/Application.java
+++ cameltest/src/main/java/com/capgemini/microservice/Application.java
@@ -1,7 +1,11 @@
 package com.capgemini.microservice;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,9 +24,20 @@ public class Application {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/greeting").allowedOrigins("http://localhost:8080");
+                registry.addMapping("/greeting").allowedOrigins("http://localhost:9000");
             }
         };
+    }
+    
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer()
+    {
+    	TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory ();
+    	
+    	factory.setPort(9000);
+    	factory.setSessionTimeout(10,TimeUnit.MINUTES);
+//    	factory.addErrorPages(new ErrorPage());
+    	return factory;
     }
 
 }
